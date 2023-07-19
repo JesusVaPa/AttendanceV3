8342e4eada97d99e24433d3827b80c2bf102e0b9
JesusVaPa <vapa.jesusangel@gmail.com>
2023-07-19 17:35:15 -0400

    d

---
+++ gui/professors.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+class Professor(tk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        # Widgets and components for Page One
+        label = tk.Label(self, text="Attendance")
+        label.pack(pady=20)
